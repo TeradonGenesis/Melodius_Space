fee8c81c0f0514886f33f30db29143b589754893
Marc <marcchai123@gmail.com>
2019-05-28 09:12:34 +0800

    fixed ui
    mc fixed ui for level selection

--- Melodius Space/Assets/UniBulletHell/Example/Script/AudioController.cs
+++ Melodius Space/Assets/UniBulletHell/Example/Script/AudioController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioController : MonoBehaviour
 {
     AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
