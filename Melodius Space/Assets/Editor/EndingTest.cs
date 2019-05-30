b4d71c417dfa1256d11aaae0d19b7fb4992f7b80
Marc <marcchai123@gmail.com>
2019-05-30 14:21:47 +0800

    unit testing
    included unit testing

---
+++ Melodius Space/Assets/Editor/EndingTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.UI;
+
+public class EndingTest
+{
+    // Start is called before the first frame update
+    [Test]
+    public void EndingScreenTest() {
+        UbhManager checkplaying;
+        var obj = new GameObject();
+        checkplaying = obj.AddComponent<UbhManager>();
+        bool isplaying = checkplaying.IsPlaying();
+        Debug.Log(isplaying);
+        Assert.IsTrue(isplaying);
+    }
+
+    [Test]
+    public void CurrentWaveTest() {
+        UbhEmitter _Emitter;
+        var obj = new GameObject();
+        _Emitter = obj.AddComponent<UbhEmitter>();
+        Assert.AreEqual(_Emitter.CurrentWave,1);
+    }
+
+    [Test]
+    public void PauseMenuTest() {
+        UbhManager checkplaying;
+        PauseMenu checkpause;
+        var obj = new GameObject();
+
+        checkplaying = obj.AddComponent<UbhManager>();
+
+        bool _gameispaused = PauseMenu.GameIsPaused;
+        bool _isplaying = checkplaying.IsPlaying();
+        Assert.AreNotEqual(_isplaying,_gameispaused);
+    }
+}
